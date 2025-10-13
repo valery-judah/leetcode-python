e722803894a120eb815ee6b05e5b98e0b84de9f8
valery <tec.valery@gmail.com>
2025-10-13 08:46:17 -0300

    feat: 0344

--- problems/0344-reverse-string/solutions.py
+++ problems/0344-reverse-string/solutions.py
@@ -6,7 +6,14 @@ import pytest
 
 
 class Baseline:
-    def solve(self, s: list[str]) -> None: ...
+    def solve(self, s: list[str]) -> None: 
+        if len(s) < 2:
+            return None
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
 
 
 class Optimized:
@@ -17,8 +24,8 @@ class Optimized:
 ALL_SOLUTIONS = [Baseline, Optimized]
 
 TEST_CASES = [
-    ("types", (["a"],), None),
-    ("empty_list", ([],), None),
+    ("egde_single", (["a"],), None),
+    ("happy_path", (["a", "b", "c", "d"],), None),
 ]
 
 
