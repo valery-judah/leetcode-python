72248b6d8bdfe4cacc7cb4712aa293a46509d2bd
valery-judah <valery.judah@gmail.com>
2025-10-17 20:33:24 -0300

    0704-binary-search-tests

--- problems/0704-binary-search/solutions.py
+++ problems/0704-binary-search/solutions.py
@@ -17,8 +17,16 @@ class Optimized:
 ALL_SOLUTIONS = [Baseline, Optimized]
 
 TEST_CASES = [
-    ("types", ([0], 0), 0),
-    ("empty_list", ([], 0), 0),
+    ("single_element_found", ([5], 5), 0),
+    ("single_element_missing", ([5], 4), -1),
+    ("target_at_left", ([1, 2, 3, 4, 5], 1), 0),
+    ("target_at_right", ([1, 2, 3, 4, 5], 5), 4),
+    ("target_in_middle", ([1, 3, 5, 7, 9], 7), 3),
+    ("missing_between_values", ([1, 3, 5, 7, 9], 4), -1),
+    ("all_negative_numbers", ([-10, -3, 0, 5, 9], -3), 1),
+    ("target_below_range", ([2, 4, 6, 8, 10], 1), -1),
+    ("target_above_range", ([2, 4, 6, 8, 10], 12), -1),
+    ("empty_array", ([], 0), -1),
 ]
 
 
